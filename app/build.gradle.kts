131a4c9bdf8af0aa19dad7238b93aa76d508a8e9
MaxGB2605 <maximbrazhko@gmail.com>
2025-03-30 14:00:23 -0400

    Part 01

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.myweatherapp2"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.myweatherapp2"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures{
+        viewBinding=true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //Scalable Size Unit (support for different screen sizes)
+    // https://mvnrepository.com/artifact/com.intuit.sdp/sdp-android
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+    implementation("com.intuit.ssp:ssp-android:1.1.0")
+
+    //Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
+
+    //Kotlin (dependency injector)
+    implementation("io.insert-koin:koin-android:3.4.2")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //View model
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+
+    //Coil
+    implementation("io.coil-kt:coil:2.5.0")
+
+
+}
---
+++ app/src/main/java/com/example/myweatherapp2/fragments/location/LocationFragment.kt
@@ -0,0 +1,6 @@
+package com.example.myweatherapp2.fragments.location
+
+import androidx.fragment.app.Fragment
+
+class LocationFragment : Fragment(){
+}
